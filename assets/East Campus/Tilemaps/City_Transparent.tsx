6cd2ecc613c1729af905d60e27647cd8653d4fcb
Se1towo <zot500@york.ac.uk>
2024-04-23 12:58:59 +0100

    Adding POIs
    Added bus stop, nisa local, flowers and updated accomodation and computer science buildings

---
+++ assets/East Campus/Tilemaps/City_Transparent.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="City_Transparent" tilewidth="24" tileheight="24" tilecount="1425" columns="57">
+ <image source="../Textures/City_Transparent.png" width="1380" height="612"/>
+</tileset>
---
+++ assets/East Campus/Tilemaps/buildings2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="buildings2" tilewidth="24" tileheight="24" tilecount="975" columns="75">
+ <image source="../Textures/buildings2.png" width="1801" height="312"/>
+</tileset>
