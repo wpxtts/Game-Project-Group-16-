d5012ad95fbb15cbc0f8026ba026ca636b46f500
william <williampotts1.wp@gmail.com>
2024-04-24 18:54:56 +0100

    Update GameScreen.java

--- core/src/com/skloch/game/GameScreen.java
+++ core/src/com/skloch/game/GameScreen.java
@@ -39,7 +39,7 @@ public class GameScreen implements Screen {
     private int energy = 100;
     private int hoursStudied, hoursRecreational, hoursSlept;
     private float daySeconds = 0; // Current seconds elapsed in day
-    private int day = 8; // What day the game is on
+    private int day = 7; // What day the game is on
     private Label timeLabel, dayLabel;
     public Player player;
     private Window escapeMenu;
