ed889b01dcbe2c0f249663ca6fed7be0d2457d6c
Se1towo <zot500@york.ac.uk>
2024-05-01 12:31:07 +0100

    fixes

---
+++ tests/src/io/dimitris/gemo/tests/EventManagerTests.java
@@ -0,0 +1,19 @@
+package io.dimitris.gemo.tests;
+
+import com.badlogic.gdx.Gdx;
+import com.skloch.game.Player;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class EventManagerTests {
+
+//    @Test
+//    public void testEventExists() {
+//        assertTrue("This event doesn't exist",
+//                Gdx.files.internal(Player.atlasConfig).exists());
+//        assertTrue("This event doesn't exist",
+//                Gdx.files.internal(Player.playerSprite).exists());
+//    }
+
+}
